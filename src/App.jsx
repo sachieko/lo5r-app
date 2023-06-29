7140e14958844c57c146e4129ff4a8b97559c950
Chelsea Sachieko <arashikabane@gmail.com>
2023-06-28 19:36:13 -0600

    Add styles and format html

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import './App.scss';
+import { useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import "./App.scss";
 
 const App = function() {
-  const [searchItem, setSearchItem] = useState('')
+  const [searchItem, setSearchItem] = useState("")
 
 
   return (
     <>
       <nav>
-        <Link to='/'><p className='title'>App Header</p></Link>
+        <Link to="/"><p className="title">App Header</p></Link>
         <form onSubmit={(e) => e.preventDefault()}>
           <span>Search: </span>
-          <input type="text" onInput={(e) => setSearchItem(e.target.value)}></input>
+          <div className="gradient-border">
+            <input type="text" className="gradient-content" onInput={(e) => setSearchItem(e.target.value)}></input>
+          </div>
         </form>
       </nav>
-      <div className="card">
-      </div>
       <div id="view">
         <Outlet />
       </div>
