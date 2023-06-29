2d53d199a4c169cc7f48b5af71bec5531ac6901b
Chelsea Sachieko <arashikabane@gmail.com>
2023-06-28 21:56:54 -0600

    simplify search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 export const Search = function() {
   const [searchItem, setSearchItem] = useState("");
-  
+
   return(
     <form onSubmit={(e) => e.preventDefault()}>
       <span>Search: </span>
-      <div className="gradient-border">
-        <input type="text" className="gradient-content" onInput={(e) => setSearchItem(e.target.value)}></input>
-      </div>
+        <input 
+          type="search" 
+          className="search" 
+          onInput={(e) => setSearchItem(e.target.value)} 
+          spellCheck="false"
+            />
     </form>
   )
 };
