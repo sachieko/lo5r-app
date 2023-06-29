599e3373d22f33348e78bf88d63c4784f7c89821
Chelsea Sachieko <arashikabane@gmail.com>
2023-06-28 20:03:24 -0600

    Update some minor styles, add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./App.scss";
+import { Search } from "./components/Search";
 
 const App = function() {
-  const [searchItem, setSearchItem] = useState("")
-
 
   return (
     <>
       <nav>
         <Link to="/"><p className="title">App Header</p></Link>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <span>Search: </span>
-          <div className="gradient-border">
-            <input type="text" className="gradient-content" onInput={(e) => setSearchItem(e.target.value)}></input>
-          </div>
-        </form>
+        <Search />
       </nav>
       <div id="view">
         <Outlet />
