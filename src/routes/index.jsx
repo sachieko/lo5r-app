6092f162fe616834d76e1a38d7aeee25be395933
Chelsea Sachieko <arashikabane@gmail.com>
2023-06-28 21:57:39 -0600

    Add styles for items and populate menu of item cards

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,14 @@
 import { ItemCard } from "../components/ItemCard";
+import menucards from '../assets/menu.json';
 
 export const Index = function() {
+    const items = menucards.map((item, index) => {
+        return <ItemCard key={index} title={item.title} desc={item.desc} url={item.url}/>
+    });
+
     return (
         <>
-        <ItemCard 
-            title="Here's a title!" 
-            desc={`Here's a big ol long description, it's supposed to be a gigantic string. So it will have a few paragraphs and say ipsum lorat or some other stuff.
-            If it has a random indentation how will that look? It will not show. Thanks html!
-            Did you know that kangaroos can't hop backwards? Now you do. Thanks snapple.`}
-        />
+        {items}
         </>
     );
 };
