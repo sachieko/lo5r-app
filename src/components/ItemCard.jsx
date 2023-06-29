f1054c6c7c8d23462eafe9c5959ee75b862cff49
Chelsea Sachieko <arashikabane@gmail.com>
2023-06-28 19:35:10 -0600

    Add Item card and styles for card

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import './ItemCard.scss';
+
+export const ItemCard = function({title, desc, url}) {
+  const parablocks = desc.split('\n').map((para, index) => <p key={index}>{para}</p>);
+
+  return (
+    <div className="card">
+      <Link to={url}>
+        <div className="title">{title}</div>
+      </Link>
+      <div className="desc">
+        {parablocks}
+      </div>
+    </div>
+  );
+};
