ed509a22df69a1d9c110430fdea68ac447a25660
Chelsea Sachieko <arashikabane@gmail.com>
2023-07-26 00:58:21 -0600

    Rename property from content to match API, add comment

--- src/routes/Questions.jsx
+++ src/routes/Questions.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { ItemCard } from "../components/ItemCard";
+import { ItemCard } from '../components/ItemCard';
+// Change this to url that hosts the API
 const APIURL = 'http://localhost:8080/questions'
 
 export const Questions = function() {
@@ -10,14 +11,13 @@ export const Questions = function() {
     if (!questions.length) {
       axios.get(APIURL)
         .then(res => {
-          setQuestions(res.data.map(question => <ItemCard key={question.id} title={question.title} desc={question.info} />));
+          setQuestions(res.data.map(question => <ItemCard key={question.id} title={question.title} desc={question.detail} />));
         })
         .catch(err => {
           console.error('Error: ', err);
           setQuestions(<ItemCard key={1} title={'An Error Occurred'} desc={'The server is likely down at the moment.'} />)
         });
     }
-
   },[]);
 
   return (
