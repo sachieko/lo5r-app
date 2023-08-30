51231821e6e83b6502f97ddffc08e023f15dd8b3
Chelsea Sachieko <arashikabane@gmail.com>
2023-08-30 01:08:25 -0600

    Update keywords for new API

--- src/helpers/keywordMap.ts
+++ src/helpers/keywordMap.ts
@@ -5,13 +5,13 @@ export interface IkeywordToUrlMap {
 export const keywordToUrlMap: IkeywordToUrlMap = {
   bushido: '/lore/1',
   compassion: '/lore/1',
-  honor: '/lore/1',
   courage: '/lore/1',
   courtesy: '/lore/1',
   respect: '/lore/1',
-  duty: '/lore/1',
   righteousness: '/lore/1',
   sincerity: '/lore/1',
-  crab: '/lore/',
-  skill: '/rules/3'
+  skill: '/rules/3',
+  roll: '/rules/1',
+  dice: '/rules/1',
+  conflict: '/rules/2'
 };
