1d01c490461b871325132bc1a28ac2b6dc751932
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-04 14:49:51 -0600

    Make bushido keywords less aggressive for now

--- src/helpers/keywordMap.ts
+++ src/helpers/keywordMap.ts
@@ -5,15 +5,8 @@ export interface IkeywordToUrlMap {
 export const keywordToUrlMap: IkeywordToUrlMap = {
   // lore 1
   bushido: '/lore/1',
-  compassion: '/lore/1',
-  courage: '/lore/1',
-  courtesy: '/lore/1',
-  respect: '/lore/1',
-  righteousness: '/lore/1',
-  sincerity: '/lore/1',
   // rules 1
-  rolls: '/rules/1',
-  dice: '/rules/1',
+  roll: '/rules/1',
   // rules 2
   conflict: '/rules/2',
   // rules 3
