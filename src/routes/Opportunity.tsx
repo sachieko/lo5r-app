dbebd75e67f77436fdce3f21af78c06c2a67f7b8
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-10 20:01:43 -0600

    Allow users to filter opportunities by search, update styles

--- src/routes/Opportunity.tsx
+++ src/routes/Opportunity.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { useOpportunities } from "../helpers/useOpportunities";
 import { IOpportunity } from "../helpers/interfaces";
 import { Table, TableColumn } from "../components/Table";
 import './Opportunity.scss';
+import { SearchBar } from "../components/SearchBar";
 
 export const Opportunity = function() {
+  const [filterWord, setFilterWord] = useState<string>('');
   const opps = useOpportunities();
 
   // Define the columns we'd like for the table from the opportunity data type
@@ -29,10 +32,33 @@ export const Opportunity = function() {
       header: 'Technique'
     }
   ];
+  const filterTable = (arr: any[], keyword: string): IOpportunity[] => {
+    const result = arr.filter(opportunity => {
+      for (const element of columns) {
+        if (opportunity[element.key]?.toLowerCase().includes(keyword)) {
+          return true;
+        }
+      }
+      return false;
+    });
+    return result;
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newFilterWord = event.target.value;
+    setFilterWord(newFilterWord);
+  };
 
   return (
     <div className='table-container'>
-      <Table data={opps} columns={columns} />
+    <SearchBar 
+      title='Filter:'
+      value={filterWord}
+      onChange={handleChange}
+      onFocus={() => {}}
+      onBlur={() => {}}
+    />
+      <Table data={filterWord ? (filterTable(opps, filterWord.toLowerCase())) : (opps)} columns={columns} />
     </div>
   );
 };
