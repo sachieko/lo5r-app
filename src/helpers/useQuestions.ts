4204a976fdc7977d4ac7e7d659d3e9190fe6a073
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-24 15:31:42 -0600

    Update types with T prefix

--- src/helpers/useQuestions.ts
+++ src/helpers/useQuestions.ts
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { IQuestion } from "./interfaces";
+import { TQuestion } from "./types";
 const APIURL: string = import.meta.env.VITE_API_URL;
 
 export const useQuestions = function () {
-  const [questions, setQuestions] = useState<IQuestion[]>([]);
+  const [questions, setQuestions] = useState<TQuestion[]>([]);
   useEffect(() => {
     const getQuestions = async function () {
       try {
         const results = await axios.get(`${APIURL}/questions/`);
-        const data: IQuestion[] = results.data; // Assuming the API response is an array of IQuestion
+        const data: TQuestion[] = results.data; // Assuming the API response is an array of IQuestion
         setQuestions(data); // Update the state with the resolved data
       } catch (error) {
         console.error("Error fetching data:", error);
