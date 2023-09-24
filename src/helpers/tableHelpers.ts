9c58c7b5443830481e7af051008ef150ac1ce4bd
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-24 13:54:38 -0600

    Update filtertable documentation to state that it is no longer case sensitive"

--- src/helpers/tableHelpers.ts
+++ src/helpers/tableHelpers.ts
@@ -7,7 +7,7 @@ The generic type is because the objects in the API change for different routes.
 It then looks at each property in the objects that matches a table column and if there is a match for the keyword in
 the object of arr, it will be included in the result array. This allows the client to filter items in the table via search.
 
-Note: It is case sensitive because smart users will be able to use it more effectively and I want to reward them over naive users.
+Note: Not case sensitive because it's inconvenient for mobile users
 */
 
 export const filterTable = <T, K extends keyof T>(
