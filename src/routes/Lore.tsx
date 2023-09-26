cf44f82d1159f6cdcba5770ae4a341921b01e360
Chelsea Sachieko <arashikabane@gmail.com>
2023-09-25 18:37:27 -0600

    Add fade element handling for Lore and Roles with useEffect, introduces card size error for these pages but will fix later

--- src/routes/Lore.tsx
+++ src/routes/Lore.tsx
@@ -3,6 +3,7 @@ import { ItemCard } from "../components/ItemCard";
 import { getLore } from "../helpers/loreHelpers";
 import FetchedParagraphs from "../helpers/LinkParser";
 import { TLore } from "../helpers/types";
+import { useEffect, useState } from "react";
 
 export async function loader({ params }: { params: Params<string> }) {
   const loreData = await getLore(params.loreId);
@@ -11,27 +12,33 @@ export async function loader({ params }: { params: Params<string> }) {
 
 export default function Lore() {
   const lore = useLoaderData() as TLore;
+  const [fadeIn, setFadeIn] = useState<boolean>(true);
+  // To allow fade in of UI elements
+  useEffect(() => {
+    setFadeIn(false);
+    const timeoutId = setTimeout(() => {
+      setFadeIn(true);
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [setFadeIn]);
 
   // All lore will have the title and detail sent back, but this is inefficient and will be refactored later on the API end.
   const title = lore.title;
   const details = FetchedParagraphs(lore.detail.split("\n"));
 
   const cards = lore.cards.map((card) => {
-    return (
-      <ItemCard
-        key={card.id}
-        title={card.header}
-        desc={card.content}
-      />
-    );
+    return <ItemCard key={card.id} title={card.header} desc={card.content} />;
   });
 
   return (
     <>
-      <div className="card">
-        <div className="title">{title}</div>
-        <div className="desc">{details}</div>
-        {cards}
+      <div className={`fadeElement ${fadeIn ? "fade" : ""}`}>
+        <div className="card">
+          <div className="title">{title}</div>
+          <div className="desc">{details}</div>
+          {cards}
+        </div>
       </div>
     </>
   );
