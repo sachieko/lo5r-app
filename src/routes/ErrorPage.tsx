3756a2d514c2037f37823fc6f9dbf6f360ec8eb4
Chelsea Sachieko <arashikabane@gmail.com>
2024-03-08 17:17:12 -0700

    Update example, fix routeError response message

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   const error = useRouteError();
   let errorMessage: string;
   if (isRouteErrorResponse(error)) {
-    errorMessage = error.error?.message || error.statusText;
+    errorMessage = error.statusText;
   } else if (error instanceof Error) {
     errorMessage = error.message;
   } else if (typeof error === "string") {
