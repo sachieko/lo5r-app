067676e5cfbd128baf99c25292fef80f80231059
Chelsea Sachieko <arashikabane@gmail.com>
2024-07-12 09:02:31 -0600

    Update keyword mappings with terrain and conditions

--- src/helpers/keywordMap.ts
+++ src/helpers/keywordMap.ts
@@ -4,23 +4,45 @@ export interface IkeywordToUrlMap {
 
 export const keywordToUrlMap: IkeywordToUrlMap = {
   // lore 1
-  bushido: "/lore/1",
-  // rules 1
-  roll: "/rules/1",
+  honor: "/lore/1",
   // rules 2
   conflict: "/rules/2",
-  // rules 3
-  skill: "/rules/3",
   // lore 2
   koku: "/lore/2",
-  currency: "/lore/2",
   // rules 4
-  duels: "/rules/4",
   duel: "/rules/4",
   // rules 5
   skirmish: "/rules/5",
   // rules 6
   intrigue: "/rules/6",
   // rules 7
-  battles: "/rules/7"
+  battles: "/rules/7",
+  // Conditions
+  afflicted: "/conditions/1",
+  bleeding: "/conditions/2",
+  burning: "/conditions/3",
+  compromised: "/conditions/4",
+  dazed: "/conditions/5",
+  disoriented: "/conditions/6",
+  dying: "/conditions/7",
+  enraged: "/conditions/8",
+  exhausted: "/conditions/9",
+  immobilized: "/conditions/10",
+  incapacitated: "/conditions/11",
+  intoxicated: "/conditions/12",
+  prone: "/conditions/13",
+  silenced: "/conditions/14",
+  unconscious: "/conditions/15",
+  wounded: "/conditions/16",
+  // terrain
+  dangerous: "/terrains/1",
+  defiled: "/terrains/2",
+  entangling: "/terrains/3",
+  hallowed: "/terrains/4",
+  imbalanced: "/terrains/5",
+  obscuring: "/terrains/6",
+  confining: "terrains/7",
+  elevated: "/terrains/8",
+  open: "/terrains/9",
+  recessed: "/terrains/10",
 };
