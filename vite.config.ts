74f40279529ad42b35c296cb70e9085e55bc6b5b
Chelsea Sachieko <arashikabane@gmail.com>
2024-10-25 07:25:02 -0600

    Update sass usage, prepare for vite to eventually use the modern js api for sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,12 @@ export default defineConfig({
   server: {
     port: 8000,
   },
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        silenceDeprecations: ["legacy-js-api"],
+      },
+    },
+  },
+});
