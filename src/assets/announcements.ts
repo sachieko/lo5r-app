53b75b6ec3e5b11b25f167797da81b3ee3c477b1
Chelsea Sachieko <dev@yuseiko.org>
2025-06-29 19:11:40 -0600

    Update announcements

--- src/assets/announcements.ts
+++ src/assets/announcements.ts
@@ -1,11 +1,16 @@
 const announcements = [
+    {
+    title: "Unplanned Downtime",
+    text: `Due to network upgrades, the system was taken offline for a few hours on the 28th and 29th. Apologies to those impacted.`,
+    date: "29/6/2025"
+  },
   {
     title: "Latest Dev Update: Core Techniques Done!",
     text: `All Core Rulebook Techniques have reference information now! UI for techniques was improved slightly, and Critical Strikes also have a rules entry along with my playtested recommendation so critical strikes feel more meaningful. Keyword links added to and open a new tab instead of previous functionality.
     Next in Development: Unmasking rule entry with custom examples, Mass Battle Objectives rule entry, Armor table, add Ring type for Techniques table, and Advantage and Disadvantage quick reference table. Collecting feedback of how keyword links work currently compared to prev.
     You can close this announcement with a click.`,
     date: "3/6/2025",
-  },
+  }
 ];
 
 export default announcements;
