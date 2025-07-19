b62dbec50222227ef5b96e992fd840d02513f148
Chelsea Sachieko <dev@yuseiko.org>
2025-07-19 00:53:51 -0600

    Update with downtime announce

--- src/assets/announcements.ts
+++ src/assets/announcements.ts
@@ -1,13 +1,13 @@
 const announcements = [
   {
+    title: "Planned Downtime:",
+    text: `Downtime is planned for network upgrades on July 24, tables will be inaccessible for a few hours on this day.`,
+    date: "7/7/2025",
+  },
+  {
     title: "Dev Update:",
     text: `Table filters have been improved so that numbers can be used to filter techniques by rank accurately, and an error involving filtering by ring on opportunities was fixed.`,
     date: "5/7/2025",
-  },
-  {
-    title: "Unplanned Downtime",
-    text: `Due to network upgrades, the system was taken offline for a few hours.`,
-    date: "4/7/2025",
   },
 ];
 
