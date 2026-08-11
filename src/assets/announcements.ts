16c952b1b5e9da6b867592574c6bda5db4d11932
Chelsea Sachieko <dev@yuseiko.org>
2026-08-11 11:22:05 -0600

    Fix announcement formatting

--- src/assets/announcements.ts
+++ src/assets/announcements.ts
@@ -2,13 +2,12 @@ const announcements = [
   {
     title: "Sneaky Update!",
     text: `Sudden Updates. Sneaky Updates. Ninjutsu Updates! 
-
-    * Spacing has been improved on a lot of entries.
-    * Techniques have gotten a bit of an overhaul, with more searchable fields and sub-categorization such as General Kata vs Close Combat Kata, but tons more. These are not exhaustive, I add them myself for your convenience. If you have specific requests just @me somehow.
-    * Courts of Stone has had all of its Techniques added finally! Shinobi rejoice! They also do utilise the above improvements too!
-    * There's been some review of older material to make sure it is cohesive.
-    * Some technical backend updates to keep current for security. These had some major changes, so that's the big news on my end. Table views should be more reliable on all devices and less variable.
-    * Advantages and Disadvantages are still planned but this took priority since it was overdue, but more improvements to existing content are planned.
+    Spacing has been improved on a lot of entries.
+    \nTechniques have gotten a bit of an overhaul, with more searchable fields and sub-categorization such as General Kata vs Close Combat Kata, but tons more. \nThese are not exhaustive, I add them myself for your convenience. If you have specific requests just @me somehow.
+    \nCourts of Stone has had all of its Techniques added finally! Shinobi rejoice! They also do utilise the above improvements too!
+    \nThere's been some review of older material to make sure it is cohesive.
+    \nSome technical backend updates to keep current for security. These had some major changes, so that's the big news on my end.\nTable views should be more reliable on all devices and less variable.
+    \nAdvantages and Disadvantages are still planned but this took priority since it was overdue, but more improvements to existing content are planned.
     
     ファイティング!! - 清子`,
     date: "11/8/2026",
